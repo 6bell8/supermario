2a492fb1e85898876edde1cf852842a0dc9c9d1b
par jin sung <parkgutime@gmail.com>
2022-10-03 17:30:34 +0900

    commit

--- index.js
+++ index.js
@@ -3,56 +3,47 @@ const app = express();
 const dotenv = require("dotenv").config();
 const path = require("path");
 const multer = require("multer");
-const cors = require("cors");
 const cloudinary = require("cloudinary");
 const MongoClient = require("mongodb").MongoClient;
-let db = null;
-
+const cors = require("cors");
 app.use(cors());
+let db = null;
+MongoClient.connect(
+  process.env.MONGO_URL,
+  { useUnifiedTopology: true },
+  (err, client) => {
+    if (err) {
+      console.log(err);
+    }
+    db = client.db("crudapp");
+  }
+);
+
 app.set("view engine", "ejs");
-app.use(express.urlencoded({ extended: false })); // post에서 보낸 데이터 req.body로 받을려면 있어야함
+app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "/public")));
 app.use("/upload", express.static(path.join(__dirname, "/upload")));
 app.set("port", process.env.PORT || 8099);
 const PORT = app.get("port");
 
-MongoClient.connect(`mongodb+srv://parkgutime:${process.env.Mongo_URL}@cluster0.jmdlgc1.mongodb.net/?retryWrites=true&w=majority`, { useUnifiedTopology: true }, (err, client) => {
-  if (err) {
-    console.log(err);
-  }
-  db = client.db("crud");
-  // console.log("db", db);
-});
-
-module.exports = db;
-
 cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_COUND_NAME,
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
   api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
-const storage = multer.diskStorage({
-  // destination: (req, file, done) => {
-  //   done(null, path.join(__dirname, "/upload"));
-  // },
-  // filename: (req, file, done) => {
-  //   done(null, file.originalname);
-  // },
-});
+const storage = multer.diskStorage({});
 
 const fileUpload = multer({ storage: storage });
 
 app.get("/", (req, res) => {
-  res.render("index", { title: "슈퍼마리오!" });
+  res.render("index", { title: "우당탕탕 우영우..." });
 });
-
 app.get("/insert", (req, res) => {
   res.render("insert", { title: "글쓰기" });
 });
-
-//여기선 입력만 되면 된다. name으로 통용
 app.post("/register", fileUpload.single("image"), (req, res) => {
+  console.log(req.file);
   cloudinary.uploader.upload(req.file.path, (result) => {
     const title = req.body.title;
     const bg = req.body.bg;
@@ -68,10 +59,9 @@ app.post("/register", fileUpload.single("image"), (req, res) => {
       category: category,
       image: image,
     });
-    res.send("잘들어갔습니다요.");
+    res.send("잘들어갔습니다.");
   });
 });
-
 app.get("/mario", (req, res) => {
   db.collection("supermario")
     .find({ category: "mario" })
@@ -79,7 +69,6 @@ app.get("/mario", (req, res) => {
       res.json({ items: result });
     });
 });
-
 app.get("/monster", (req, res) => {
   db.collection("supermario")
     .find({ category: "monster" })
@@ -87,7 +76,6 @@ app.get("/monster", (req, res) => {
       res.json({ items: result });
     });
 });
-
 app.get("/all", (req, res) => {
   db.collection("supermario")
     .find()
